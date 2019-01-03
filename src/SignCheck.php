f5ac26131779853347dfbfbd2becfd03f3443765
liuling <liuling@shoplazza.com>
2019-01-03 15:49:47 +0800

    规范目录结构

--- src/SignCheck.php
+++ src/SignCheck.php
@@ -6,7 +6,7 @@
  * Time: 15:55
  */
 
-namespace liuling\sign;
+namespace Liuling\Sign;
 
 use Yii;
 use yii\base\Component;
@@ -15,7 +15,6 @@ use yii\base\InvalidConfigException;
 class SignCheck extends Component
 {
     public $secret = '';
-
     public function init()
     {
         parent::init();
